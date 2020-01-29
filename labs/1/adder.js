6c31cc245c476f20a8894b8a373f4703f57276a4
Gabrielle Sinclair <you@example.com>
2020-01-28 23:45:33 -0500

    new commit

---
+++ labs/1/adder.js
@@ -0,0 +1,19 @@
+
+
+class Adder{
+
+    constructor(){
+    this.props.a = a;
+    this.props.b = b;
+    }
+
+    sum(a,b){
+       x = (a + b);
+    }
+
+    render(){
+    return <p>The sum of" a.value "and" b.value "is" x.value"</p>;
+    }
+}
+
+module.exports = Adder ;
---
+++ labs/1/index.js
@@ -0,0 +1,5 @@
+
+const Adder = require("./adder.js");
+
+
+let a = new Adder(5,10);
