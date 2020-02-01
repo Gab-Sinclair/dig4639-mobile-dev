5b1b12cef97003dc8177c5e8cd5165209b8a1d74
Gabrielle Sinclair <you@example.com>
2020-01-31 22:10:36 -0500

    new commit

--- labs/1/adder.js
+++ labs/1/adder.js
@@ -1,20 +1,21 @@
+import React from "react";
 
+class Adder extends React.Componet{
 
-class Adder{
-
-    constructor(){
+    constructor(a,b){
     this.props.a = a;
     this.props.b = b;
+    this.props.x = x;
     }
 
     sum(a,b){
-       this.props.x = (a + b);
+       x = ({a} + {b});
+       return x;
     }
 
     render(){
-        this.sum();
+        this.sum(a,b);
           return '<p>The sum of ${this.props.a}; and ${this.props.b}; is ${this.props.x}</p>';
     }
 }
-
-module.exports = Adder ;
+module.exports = Adder;
--- labs/1/index.js
+++ labs/1/index.js
@@ -1,5 +1,8 @@
 
 const Adder = require("./adder.js");
 
+//Define new adder object 
+let a = new Adder({a:5,b:10});
 
-let a = new Adder(5,10);
+//render adder object in console
+console.log(a.render());
