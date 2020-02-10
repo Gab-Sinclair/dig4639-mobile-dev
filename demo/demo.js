154a5eb876d68909b242c428d8826339e3280323
Gabrielle Sinclair <ga023285@ucf.edu>
2020-02-10 12:28:05 -0500

    closes issue #95

---
+++ demo/demo.js
@@ -0,0 +1,58 @@
+class Pet{
+    constructor(props){
+        this.props = props;
+    }
+    getName(){
+        return this.props.name;
+    }
+   getSpecies(){
+       return this.props.species;
+   }
+   getSpeech(){
+       return "I make this sound: ";
+   }
+   getIdentity =() =>{
+       console.log(this);
+   }
+}
+
+class Cat extends Pet{
+    constructor(props){
+        //inherit props from parent
+        super(props);
+        //add props in subclass
+        this.props.species = "cat";
+    }
+    getSpeech(){
+        return super.getSpeech() + "meow";
+    }
+}
+
+class Dog extends Pet{
+    constructor(props){
+        //inherit props from parent
+        super(props);
+        //add props in subclass
+        this.props.species = "dog";
+    }
+    getSpeech(){
+        return super.getSpeech() + "ruff";
+    }
+}
+
+function sayName(pet){  //creates pet name species undefined in below example
+    console.log(`my name is ${pet.getName()}, and I am  ${pet.getSpecies()}`);
+    console.log(`${pet.getSpeech()}`);
+}
+
+var myPet = new Cat({name:"Luna"});
+sayName(myPet);
+myPet.getIdentity()
+//assign method getIdentity to VAR MyIdentity
+var myIdentity = myPet.getIdentity;
+//call function
+myIdentity();
+
+var myDog = new Dog({name:"Fido"});
+sayName(myDog);
+myDog.getIdentity()
