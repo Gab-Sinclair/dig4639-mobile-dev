e23545d173c595f827c818721ca9569ed7f10d4d
Gabrielle Sinclair <you@example.com>
2020-02-09 23:31:52 -0500

    new commit

--- labs/2/webpack-1.config.js
+++ labs/2/webpack-1.config.js
@@ -3,10 +3,16 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html'
+}),
+],
   module: {
     rules: [
        {
