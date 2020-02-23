f5f8e78e9ed205c7e30fe4802d04f0e32d1fe632
Gabrielle Sinclair <you@example.com>
2020-02-22 21:38:30 -0500

    new commit

--- labs/3/card/src/App.js
+++ labs/3/card/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/index.js'
 
 class App extends React.Component{
   render(){
     return (
-      <card/>
+      <Card/>
     );}
 }
 
--- labs/3/card/src/components/Card/index.js
+++ labs/3/card/src/components/Card/index.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import "./index.css"
 
-class Card {
+class Card extends React.Component {
 constructor (props) {
+    super(props)
     this.props = props;
     this.element = document.createElement("div");
     this.element.innerHTML = this.props.content;
