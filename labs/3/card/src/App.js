2cd10e5f8786a20113d6f7c61ded62559a813c6b
Gabrielle Sinclair <you@example.com>
2020-02-22 21:29:50 -0500

    new commit

---
+++ labs/3/card/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return (
+      <card/>
+    );}
+}
+
+export default App;
