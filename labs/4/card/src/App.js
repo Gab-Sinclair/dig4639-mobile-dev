7874639f4403adc75ba89f67e9ae51e33d015159
Gabrielle Sinclair <you@example.com>
2020-02-29 23:05:46 -0500

    updated component

--- labs/4/card/src/App.js
+++ labs/4/card/src/App.js
@@ -2,17 +2,11 @@ import React from 'react';
 import './App.css';
 import CardList from './components/CardList';
 
-function Card(props){
-  return <p className="card" onClick={() => props.removeTask(props.id)}>{props.content}</p>
-}
 
-
+  
 function App(props){
     return(
-      <div>
         <CardList/>
-        </div>
-        
   
     );
   }
--- labs/4/card/src/components/CardList/index.js
+++ labs/4/card/src/components/CardList/index.js
@@ -1,59 +1,67 @@
 import React from 'react';
-import Data from "./data.json";
+import data from "./data.json";
 
+
+//TodoItem to pass properties 
 function Card(props){
-    return <p className="card" onClick={() => props.removeTask(props.id)}>{props.content}</p>
+  //show the property . content on the card passed in to TodoItem
+    return <p className="card" onClick={() => props.removeTask(props.id)}>{props.title}<br></br>{props.content} </p>
+}
+
+
+class CardList extends React.Component{
+  constructor(props){
+    super(props)
+    //track state changes in this component
+    this.state ={
+      //manage json file
+      data, 
+      hideTasks:false,
+    }
+    this.currentId = 4;
   }
 
- class CardList extends React.Component{
-     constructor(props){
-         super(props)
-         this.state ={
-            //manage json file
-            Data
-         }
-     }
- //add task by click event
- addItem(e) {
-    let Data = this.state.Data
-    Data.push(
-     {"id": this.currentId, "content" :this.refs.cardContent.value, "title": this.refs.CardTitle.value })
-      this.currentId++
-      this.setState({Data:Data})   
-      this.refs.cardContent.value = ""     
-}
- 
-
-// Remove Card funcion with x button and class close Pass a click listener function so that each card can remove themselves.
+  //add task by click event
+    addTask(e) {
+      console.log(this.refs.taskContent)
+      let data = this.state.data
+      data.push(
+       {"id": this.currentId, "content" :this.refs.taskContent.value, "title": this.refs.title.value, "completed" :true})
+        this.currentId++
+        this.setState({data:data})   
+        this.refs.taskContent.value = ""     
+        this.refs.title.value =""
+  }
 //remove task by id
-removeItem(id){
-    let Data = this.state.Data
-    Data = Data.filter((v) =>  v.id !== id)
-    this.setState({Data})
+  removeTask(id){
+    let data = this.state.data
+    data = data.filter((v) =>  v.id !== id)
+    this.setState({data})
   }
 
-  
   render(){
-    return (
+return (
+
+  <>
+  <input type="text" ref = "taskContent" />
+  <input type="text" ref = "title" />
+  <input type="button" value= "Add Task" onClick={(e) => this.addTask(e)} />
+ <br/>
+ <input ref = "hideTasks" type="checkbox" id="hidTasks" 
+ name="hideTasks" value="hideTasks" onChange= {(e) => this.setState({hideTasks:e.target.checked})}/>
+ <label htmlFor="hideTasks"> Clear tasks</label><br></br>
+
   
-      <>
-      <input type="text" ref = "cardContent" />
-      <input type="text" ref = "cardTitle" />
-      <input type="button" value= "Add Task" onClick={(e) => this.addItem(e)} />
-     <br/>
-
-  { this.state.Data.map((v) => 
-      <Card id ={v.id} removeTask ={(id) => this.removeItem(id)}
-        key = {v.id}
-        title = {v.title} 
-        content ={v.content}/>)}
-        </>)
-
-   }
-
+   {//show only not completed items in todolist
+    ((this.state.hideTasks) ? this.state.data
+    .filter((v) => !v.content) : this.state.data)
+    .map((v) => 
+      <Card id ={v.id} removeTask ={(id) => this.removeTask(id)}
+      key = {v.id}
+      title= {v.title}
+      completed ={v.completed}
+      content ={v.content}/>)} 
+      </>) 
+ }
 }
-//Use attributes to pass data to each Card. JSON file (As per React's warnings, each created Card should have an attribute named key with its index.)
-
-
-
-export default CardList;
+export default CardList
