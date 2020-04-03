409b2fcb1240ff32164609f0b041cd67abf2b577
Gabrielle Sinclair <you@example.com>
2020-04-02 23:46:14 -0400

    add React native and edit app.js

---
+++ labs/6/lab-6/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //init question array
+    this.state = {
+      ques:[]
+    };
+  }
+
+  componentDidMount() {
+
+    //fetch data and form json results set equal to variable
+    fetch("https://api.weather.gov/gridpoints/MLB/25,69/forecast")
+    .then(res => res.json())
+    .then((result) => {
+      let ques = result.properties.periods;
+     
+      //set results to array
+      this.setState({
+        ques: ques
+      });
+
+      console.log({ques})
+
+    })
+    .catch((error) => {console.log(error)} );
+
+  }
+ 
+
+  render(){
+  return (
+    //display data from api in a card with question and answer 
+   <View style={styles.container}>
+     {this.state.ques.map((value, index) => {
+           <Card key={index}>
+              <Text>{value.name}</Text>
+              <Text> {value.startTime}</Text>
+              {console.log({value})}
+          </Card>})} 
+
+      <Text>Open u App.js to start working on your app!</Text>
+    </View>
+  );
+}}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
