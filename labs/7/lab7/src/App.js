3fa99708b82b9da6c620fbea06638bdc216fc6c1
Gabrielle Sinclair <you@example.com>
2020-04-08 20:14:22 -0400

    starter files

---
+++ labs/7/lab7/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ContactsSee from './components/contactsSee';
+
+class App extends React.Component{
+  render(){
+  return (
+    <div>
+    <ContactsSee/>
+    </div>
+  );
+}
+}
+
+export default App;
